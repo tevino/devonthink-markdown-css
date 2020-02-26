aaa220d40a2529fb6cecf09462aa033ef06b22f3
Tevin <mail2tevin@gmail.com>
2020-02-27 00:12:40 +0800

    Use Github Actions for auto updating

--- generate.sh
+++ generate.sh
@@ -4,12 +4,3 @@ CSS_FILE=github-markdown.css
 CSS_URL="https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/$CSS_FILE" 
 
 curl "$CSS_URL" | sed 's/.markdown-body/body/g' > $CSS_FILE
-
-git config --global user.email ""
-git config --global user.name "Tevin's Bot"
-
-git add "$CSS_FILE"
-if git commit -m "Update from upstream"; then
-    git push origin master
-fi
-
