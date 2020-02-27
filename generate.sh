310420a44a0b21be7482a763e76f11834ea77852
Tevin Zhang <mail2tevin@gmail.com>
2020-02-27 12:19:47 +0800

    Refine generate.sh

--- generate.sh
+++ generate.sh
@@ -3,4 +3,4 @@
 CSS_FILE=github-markdown.css
 CSS_URL="https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/$CSS_FILE" 
 
-curl "$CSS_URL" | sed 's/.markdown-body/body/g' > $CSS_FILE
+curl "$CSS_URL" | sed 's/\.markdown-body/body/g' > $CSS_FILE
